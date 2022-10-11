dd1b6e8d5307bd3fa8ce90248793ca84a3bed7d4
Rati Sonkar <73036667+rati630@users.noreply.github.com>
2022-10-11 16:21:29 +0530

    Update ContactList.js

--- ContactList.js
+++ ContactList.js
@@ -3,9 +3,13 @@ import ContactCard from "./ContactCard";
 
 
 const ContactList = (props)=>{
+
+    const deleteContactHandler = (id) =>{
+        props.getContactId(id);
+    };
     const renderContactList = props.contacts.map((contact)=>{
         return (
-            <ContactCard contact={contact}></ContactCard>
+            <ContactCard contact={contact} clickHandler = {deleteContactHandler}></ContactCard>
         );
     })
     return(
